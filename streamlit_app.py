5a8536dc4296742067d06ee980d17be9904dacd9
Valle-he <valentin_helm@web.de>
2024-07-01 13:55:32 +0000

    Total Debt (Million $)

--- streamlit_app.py
+++ streamlit_app.py
@@ -376,7 +376,7 @@ if st.sidebar.button("Analyze Stock"):
             'Profitability Margins': ['Profit Margins', 'Gross Margins', 'EBITDA Margins', 'Operating Margins'],
             'Financial Metrics': ['Return on Assets (ROA)', 'Return on Equity (ROE)'],
             'Revenue Metrics': ['Revenue Growth', 'Total Revenue (Million $)', 'Total Revenue per Share','Gross Profits'],
-            'Financial Health': ['Total Debt (Million $)','Debt to Equity Ratio', 'Current Ratio'],
+            'Financial Health': ['Total Debt (Million $)', 'Debt to Equity Ratio', 'Current Ratio'],
             'Cashflow Metrics': ['Total Cash (Million $)', 'Operating Cashflow (Million $)', 'Levered Free Cashflow (Million $)'],
         }
         
